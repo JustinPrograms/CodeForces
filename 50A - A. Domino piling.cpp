4fb06b6f99a2ffbc705f532eece0c3eef4cd8fbd
Justin <83477376+JustinFire@users.noreply.github.com>
2021-10-30 23:01:48 -0400

    Create 50A - A. Domino piling.cpp

---
+++ 50A - A. Domino piling.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+void solve(){
+
+    int m, n;
+
+    cin >> m >> n;
+
+    cout << m * n / 2;
+}
+
+
+int main() {
+
+    solve();
+
+    return 0;
+}
