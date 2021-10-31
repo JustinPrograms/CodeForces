a219bf7080ee19de467d804bce8f0525132576e2
Justin <83477376+JustinFire@users.noreply.github.com>
2021-10-30 23:16:36 -0400

    Update 112A - A. Petya and Strings.cpp

--- 112A - A. Petya and Strings.cpp
+++ 112A - A. Petya and Strings.cpp
@@ -1,44 +1,63 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
+
+
 
 using namespace std;
 
-
-void solve() {
-
-    string str1, str2;
-
-    cin >> str1 >> str2;
-
-    for (int i = 0; i < str1.size(); i++) {
-
-        if (str1[i] < 92) {
-            str1[i] += 32;
-        }
-
-        if (str2[i] < 92) {
-            str2[i] += 32;
-        }
-
-    }
-
-    if (str1 < str2) {
-        cout << -1;
-
-    } else if (str1 > str2) {
-        cout << 1;
-
-    } else {
-        cout << 0;
-    }
-
-}
+void solve();
 
 int main() {
-
     solve();
-
 
     return 0;
 }
+
+void solve() {
+
+    string s1;
+    string s2;
+
+    cin >> s1;
+    cin >> s2;
+
+    string s1New;
+    string s2New;
+
+
+    for (int i = 0; i < s1.length(); i++) {
+        if (int(s1[i]) > 90) {
+            s1New += int(s1[i]) - 32;
+        } else {
+            s1New += int(s1[i]);
+        }
+
+        if (int(s2[i]) > 90) {
+            s2New += int(s2[i]) - 32;
+        } else {
+            s2New += int(s2[i]);
+        }
+
+        if (int(s1New[i]) != int(s2New[i])) {
+            if (int(s1New[i]) < int(s2New[i])) {
+                cout << -1;
+                break;
+            } else if (int(s2New[i]) < int(s1New[i])) {
+                cout << 1;
+                break;
+            }
+        }
+    }
+
+    if (s1New == s2New) {
+        cout << 0;
+    }
+
+
+
+
+}
+
+ 
